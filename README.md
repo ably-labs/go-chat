c9a4a200738e2d725adbeb93449ade0300c00a3b
tomC <tc13270@my.bristol.ac.uk>
2021-02-04 14:02:12 +0000

    Update basic got chat app per feedback
    - Uses newest Ably Go library
    -- This involves various code changes to be compatable
    - Add go.mod to simplify getting started
    - Removed API key from core code for security

--- README.md
+++ README.md
@@ -4,16 +4,10 @@ Basic Go chat program using Ably for networking.
 
 ## Setup
 
-Firstly install the Ably Go library:
+Firstly you need to replace the **YOUR_API_KEY** text in `.env.example` with your Ably API key and re-name the file to `.env`. You can sign up for an account with [Ably](https://www.ably.com/) and access your API key from the [app dashboard](https://www.ably.com/accounts/any/apps/any/app_keys). We keep the API key in `.env` and ignore it in `.gitignore` to avoid accidentally sharing the API key.
+
+Next, run the application with the following line, specifying your ClientID as an argument, and you're ready to start communicating over Ably!
 
 ```bash
-~ $ go get -u github.com/ably/ably-go/ably
-```
-
-Next, replace the **INSERT_YOUR_API_KEY_HERE** line in `chat.go` with your Ably API key. You can sign up for an account with [Ably](https://www.ably.com/) and access your API key from the [app dashboard](https://www.ably.com/accounts/any/apps/any/app_keys).
-
-Finally, run the application with the following line, specify your ClientID as when asked, and you're ready to start communicating over Ably!
-
-```bash
-~ $ go run chat.go
+~ $ go run chat.go YOUR_USERNAME
 ```
--- chat.go
+++ chat.go
@@ -2,107 +2,96 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"os"
 	"strings"
 
 	"github.com/ably/ably-go/ably"
 	"github.com/ably/ably-go/ably/proto"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Type your clientID")
-	reader := bufio.NewReader(os.Stdin)
-	clientId, _ := reader.ReadString('\n')
-	clientId = strings.Replace(clientId, "\n", "", -1)
+	godotenv.Load()
 
-	opts := &ably.ClientOptions{
-		AuthOptions: ably.AuthOptions{
-			// If you have an Ably account, you can find
-			// your API key at https://www.ably.io/accounts/any/apps/any/app_keys
-			Key: "INSERT_YOUR_API_KEY_HERE",
-		},
-		ClientID: clientId,
-		// NoEcho:   true, // Uncomment to stop messages you send from being sent back
-	}
-
-	fmt.Println("You can now send messages!")
+	username := os.Args[1]
 
 	// Connect to Ably using the API key and ClientID specified above
-	client, err := ably.NewRealtimeClient(opts)
+	client, err := ably.NewRealtime(
+		ably.WithKey(os.Getenv("ABLY_KEY")),
+		// ably.WithEchoMessages(true), // Uncomment to stop messages you send from being sent back
+		ably.WithClientID(username))
 	if err != nil {
 		panic(err)
 	}
+
+	fmt.Println("You can now send messages!")
 
 	// Connect to the Ably Channel with name 'chat'
 	channel := client.Channels.Get("chat")
 
 	// Enter the Presence set of the channel
-	channel.Presence.Enter("")
+	channel.Presence.Enter(context.Background(), "")
 
-	getHistory(*channel)
+	getHistory(channel)
 
-	go subscribeToPresence(*channel)
-
-	go subscribe(*channel)
+	subscribe(channel)
 
 	// Start the goroutine to allow for publishing messages
-	publishing(*channel)
+	publishing(channel)
 }
 
-func subscribeToPresence(channel ably.RealtimeChannel) {
-	sub, err := channel.Presence.Subscribe()
-	if err != nil {
-		panic(err)
-	}
-
-	for msg := range sub.PresenceChannel() {
-		if msg.State == proto.PresenceEnter {
-			fmt.Printf("%v has entered the chat\n", msg.ClientID)
-		} else if msg.State == proto.PresenceLeave {
-			fmt.Printf("%v has left the chat\n", msg.ClientID)
-		}
-	}
-}
-
-func getHistory(channel ably.RealtimeChannel) {
+func getHistory(channel *ably.RealtimeChannel) {
 	// Before subscribing for messages, check the channel's
 	// History for any missed messages. By default a channel
 	// will keep 2 minutes of history available, but this can
 	// be extended to 48 hours
 	page, err := channel.History(nil)
-	for ; err == nil; page, err = page.Next() {
+	for ; err == nil && page != nil; page, err = page.Next() {
 		for _, msg := range page.Messages() {
 			fmt.Printf("Previous message from %v: '%v'\n", msg.ClientID, msg.Data)
 		}
 	}
 }
 
-func subscribe(channel ably.RealtimeChannel) {
-	// Initiate a subscription to the channel
-	sub, err := channel.Subscribe()
+func subscribe(channel *ably.RealtimeChannel) {
+	// Subscribe to messages sent on the channel
+	_, err := channel.SubscribeAll(context.Background(), func(msg *ably.Message) {
+		fmt.Printf("Received message from %v: '%v'\n", msg.ClientID, msg.Data)
+	})
 	if err != nil {
-		panic(err)
+		err := fmt.Errorf("subscribing to channel: %w", err)
+		fmt.Println(err)
 	}
 
-	// For each message we receive from the subscription, print it out
-	for msg := range sub.MessageChannel() {
-		fmt.Printf("Received message from %v: '%v'\n", msg.ClientID, msg.Data)
+	// Subscribe to presence events (people entering and leaving) on the channel
+	_, pErr := channel.Presence.SubscribeAll(context.Background(), func(msg *ably.PresenceMessage) {
+		if msg.Action == proto.PresenceEnter {
+			fmt.Printf("%v has entered the chat\n", msg.ClientID)
+		} else if msg.Action == proto.PresenceLeave {
+			fmt.Printf("%v has left the chat\n", msg.ClientID)
+		}
+	})
+	if pErr != nil {
+		err := fmt.Errorf("subscribing to presence in channel: %w", pErr)
+		fmt.Println(err)
 	}
 }
 
-func publishing(channel ably.RealtimeChannel) {
+func publishing(channel *ably.RealtimeChannel) {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
 		text, _ := reader.ReadString('\n')
-		text = strings.Replace(text, "\n", "", -1)
+		text = strings.ReplaceAll(text, "\n", "")
 
 		// Publish the message typed in to the Ably Channel
-		res, err := channel.Publish("message", text)
+		err := channel.Publish(context.Background(), "message", text)
 		// await confirmation that message was received by Ably
-		if err = res.Wait(); err != nil {
-			panic(err)
+		if err != nil {
+			err := fmt.Errorf("publishing to channel: %w", err)
+			fmt.Println(err)
 		}
 	}
 }
